1dd92cfa45c7f23eaef399b630d1ee4c94d10631
achilove <javoxiraka@gmail.com>
2020-02-21 10:27:03 +0500

    init: typical array problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  return array.length > 0 ? Math.min.apply(Math, array) : 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  return array.length > 0 ? Math.max.apply(Math, array): 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  return array.reduce((accumulator, current) => accumulator + current, 0) / array.length || 0;
 }
